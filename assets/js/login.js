a33a6412d078a983ba9b82acc7feffd1d591cde6
cheng2020-07 <2438344823@qq.com>
2020-07-31 20:58:18 +0800

    完成了登录和注册的开发

---
+++ assets/js/login.js
@@ -0,0 +1,88 @@
+$(function () {
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+})
+//点击""去登陆"的链接
+$(function () {
+    $('#link_login').on('click', function () {
+        $('.login-box').show()
+        $('.reg-box').hide()
+    })
+
+    // 三个步骤
+    //从layui中获取from对象
+    var form = layui.form
+    var layer = layui.layer
+    //通过form.verify()函数自定义校验规则
+    form.verify({
+        //自定义了一个叫做pwd 校验规则
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        //校验
+        // (property) repwd: (value: any) => String
+        repwd: function (value) {
+            //通过形参拿到的是确认密码框中的内容
+            // 还需要拿到密码框中的内容
+            //然后进行一次等于的判断
+            //如果判断失败，则return一个提示消息即可
+
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+                return '两次密码不一致'
+            }
+        }
+    })
+
+    //注意空格
+    //监听注册表单的提交事件
+    var data = {
+        username: $('#form_reg [name=username]').val(),
+        password: $('#form_reg [name=password]').val()
+    }
+    $('#form_reg').on('submit', function (e) {
+        e.preventDefault()
+        $.post('/api/reguser', data, function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+
+            }
+            layer.msg('注册成功，请登录！')
+            $('#link_login').click()
+
+        })
+
+    })
+
+    //监听登录表单的提交事件
+    $('#form_login').submit(function (e) {
+        //阻止默认提交行为
+        e.preventDefault()
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            //快速获取表单的数据
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+
+                    return layer.msg('登陆失败！')
+                }
+                layer.msg('登录成功！')
+
+
+                location.href = '/index.html'
+            }
+
+
+
+        })
+
+    })
+
+
+
+
+
+
+})
